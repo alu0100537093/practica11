ab7431d625a235ac7a755d603aa08060fc9fdec6
Daniel Luis Martin <alu0100537093@ull.edu.es>
2014-10-24 00:50:35 +0100

    Añadidas expectativas para que existan respuestas.

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -12,5 +12,15 @@ describe Pregunta do
 		it "La pregunta tiene enunciado" do
 			@p1.get_enunciado != nil
 		end
-  
+		
+		it "El enunciado de la pregunta es el correcto" do
+			@p1.get_enunciado = "Pregunta"
+		end
+		
+		it "Existen respuestas" do
+			@p1.get_respuesta_a != nil
+			@p1.get_respuesta_b != nil
+			@p1.get_respuesta_c != nil
+			@p1.get_respuesta_d != nil
+		end
 end
