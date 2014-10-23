f7c48f0c04cd0944cad1d2414b24381f0e9ed194
Daniel Luis Martin <alu0100537093@ull.edu.es>
2014-10-24 00:42:56 +0100

    Añadida expectativa para que exista enunciado.

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -2,8 +2,15 @@ require 'exam.rb'
 
 
 describe Pregunta do
-  before :each do
-    @p1 = Pregunta.new("Pregunta", "Respuesta a", "Respuesta b", "Respuesta c", "Respuesta d")
-  end
+  
+		
+		before :each do
+			@p1 = Pregunta.new("Pregunta", "Respuesta a", "Respuesta b", "Respuesta c", "Respuesta d")
+		end
+  
+
+		it "La pregunta tiene enunciado" do
+			@p1.get_enunciado != nil
+		end
   
 end
