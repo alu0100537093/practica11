0055b7c97a87a9e7b5ad922ce793eafc68a1a393
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-10-24 00:41:37 +0100

    Definido initialize de la clase Pregunta

--- lib/exam/preguntas.rb
+++ lib/exam/preguntas.rb
@@ -1,2 +1,10 @@
 class Pregunta
+	
+	def initialize(enunciado, respuesta_1, respuesta_2, respuesta_3, respuesta_4)
+		@enunciado = enunciado
+		@res1 = respuesta_1
+		@res2 = respuesta_2
+		@res3 = respuesta_3
+		@res4 = respuesta_4
+	end
 end
