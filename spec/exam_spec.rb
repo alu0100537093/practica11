2c63d172593a78291aefdaa79d9376ec032731fe
Daniel Luis Martin <alu0100537093@ull.edu.es>
2014-10-24 00:32:15 +0100

    Añadido el .new a la expectativa.

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -3,7 +3,7 @@ require 'exam.rb'
 
 describe Pregunta do
   before :each do
-    @p1 = Pregunta
+    @p1 = Pregunta.new("Pregunta", "Respuesta a", "Respuesta b", "Respuesta c", "Respuesta d")
   end
   
 end
