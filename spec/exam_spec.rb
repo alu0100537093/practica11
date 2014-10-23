6cf5c5fd444cc11e488891c32f96cb9d46555a33
Daniel Luis Martin <alu0100537093@ull.edu.es>
2014-10-24 00:23:33 +0100

    Modificada la expectativa de creacion del objeto.

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -3,7 +3,7 @@ require 'exam.rb'
 
 describe Pregunta do
   before :each do
-    @p1 = Pregunta.new("Pregunta", "respuesta a", "respuesta b", "respuesta c", "respuesta d")
+    @p1 = Pregunta
   end
   
 end
