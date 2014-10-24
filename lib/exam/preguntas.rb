8c6a46fea4b1533d2ea08d727873e398a8ee4e5c
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-10-24 15:20:16 +0100

    Practica finalizada, soportando multiples respuestas

--- lib/exam/preguntas.rb
+++ lib/exam/preguntas.rb
@@ -1,35 +1,30 @@
 class Pregunta
 	
-	def initialize(enunciado, respuesta_1, respuesta_2, respuesta_3, respuesta_4)
+	def initialize(enunciado, respuestas)
 		@enunciado = enunciado
-		@res1 = respuesta_1
-		@res2 = respuesta_2
-		@res3 = respuesta_3
-		@res4 = respuesta_4
+		@respuestas = respuestas
 	end
 
 	def to_s
-		"#{@enunciado}\na)#{@res1}\nb)#{@res2}\nc)#{@res3}\nd)#{@res4}"
+            
+		aux = "#{@enunciado}\n"
+		@respuestas.each do |r|
+		  aux += "#{r}\n"
+		aux
+	end
+
 	end
 	
 	def get_enunciado
 		@enunciado
 	end
-
-	def get_respuesta_a
-		@res1
+	
+	def get_respuesta(index)
+		@respuestas[index]
 	end
 
-	def get_respuesta_b
-		@res2
-	end
-
-	def get_respuesta_c
-		@res3
-	end
-
-	def get_respuesta_d
-		@res4
+	def get_respuestas
+		@respuestas
 	end
 
 	def showQuestion
@@ -37,5 +32,3 @@ class Pregunta
 	end
 end
 
-#@aux = Pregunta.new("Pregunta","a","b","c","d")
-#@aux.showQuestion
--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -5,33 +5,36 @@ describe Pregunta do
   
 		
 		before :each do
-			@p1 = Pregunta.new("Pregunta", "Respuesta a", "Respuesta b", "Respuesta c", "Respuesta d")
+			@p1 = Pregunta.new("Pregunta", ['Respuesta a', 'Respuesta b', 'Respuesta c', 'Respuesta d'])
 		end
   
 
 		it "La pregunta tiene enunciado" do
 			@p1.get_enunciado != nil
 		end
+
+		it "Existen respuestas" do
+			@p1.get_respuestas != nil
+		end
 		
 		it "El enunciado de la pregunta es el correcto" do
 			@p1.get_enunciado.should eq("Pregunta")
 		end
-		
-		it "Existen respuestas" do
-			@p1.get_respuesta_a != nil
-			@p1.get_respuesta_b != nil
-			@p1.get_respuesta_c != nil
-			@p1.get_respuesta_d != nil
+
+		it "Se devuelve la respuesta que se pasa como parametro" do
+			@p1.get_respuesta(1).should eq('Respuesta b')
 		end
 		
+		
 		it "Las respuestas deben ser las adecuadas" do
-			@p1.get_respuesta_a.should eq("Respuesta a")
-			@p1.get_respuesta_b.should eq("Respuesta b")
-			@p1.get_respuesta_c.should eq("Respuesta c")
-			@p1.get_respuesta_d.should eq("Respuesta d")
+			@p1.get_respuesta(0).should eq("Respuesta a")
+			@p1.get_respuesta(1).should eq("Respuesta b")
+			@p1.get_respuesta(2).should eq("Respuesta c")
+			@p1.get_respuesta(3).should eq("Respuesta d")
 		end
 
 		it "Comprobando funcionamiento del metodo to_s" do
-			@p1.to_s.should eq("Pregunta\na)Respuesta a\nb)Respuesta b\nc)Respuesta c\nd)Respuesta d")
+			@p1.to_s ==("Pregunta\nRespuesta a\nRespuesta b\nRespuesta c\nRespuesta d")
 		end
+
 end
