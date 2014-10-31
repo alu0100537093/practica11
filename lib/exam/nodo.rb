96b19ee5a01e2ecc164fd46e3accb8a67baa5e08
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-10-31 02:53:47 +0000

    Correcto funcionamiento del == de la struct Nodo

--- lib/exam/nodo.rb
+++ lib/exam/nodo.rb
@@ -8,4 +8,12 @@ Nodo = Struct.new(:value, :next) do
 	def get_next
 		self.to_a[1]
 	end
+
+	def ==(other)
+		if((self.to_a[0] == other.to_a[0])&& (self.to_a[1] == other.to_a[1]))
+			return true
+		else
+			return false
+		end
+	end
 end
