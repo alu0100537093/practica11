7ba896318a94efd797bfb22ced91b6fa9f6020dd
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-11-20 20:06:36 +0000

    Plantilla de respuestas de los examenes almacenada correctamente

--- lib/exam/examen.rb
+++ lib/exam/examen.rb
@@ -1,10 +1,14 @@
 class Examen
-	attr_reader :id, :lista
+	attr_reader :id, :lista, :plantilla
 
 	def initialize(id, lista)
 		raise ArgumentError, 'El id especificado no es una cadena' unless id.is_a? String
 		raise ArgumentError, 'No se le pasa como argumento un objeto de tipo Lista' unless lista.is_a? Lista
 		@id = id
 		@lista = lista
+		@plantilla = []
+		@lista.each{|x|
+			@plantilla.push(x.r_correcta)
+		}
 	end
 end
