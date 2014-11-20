7b129364cc03de55a096d7ac1f692703be2610c2
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-11-20 20:51:39 +0000

    Creado initialize de la clase interfaz

--- lib/exam/interfaz.rb
+++ lib/exam/interfaz.rb
@@ -1,2 +1,11 @@
 class Interfaz
+	
+	def initialize(examenes)
+		examenes.each{ |x|
+			raise ArgumentError, 'El array debe ser de objetos de tipo Examen' unless x.is_a? Examen
+		}
+		
+		@examenes = examenes
+		@examen_seleccionado = 0 # Este atributo apuntará al indice del examen que se desea realizar
+	end
 end
