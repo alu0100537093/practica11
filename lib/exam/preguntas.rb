797f1832e532f06dc3e414763e0e9804c2864f11
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-11-20 18:34:21 +0000

    Añadidas excepciones para que el usuario no cree incorrectamente una pregunta

--- lib/exam/preguntas.rb
+++ lib/exam/preguntas.rb
@@ -4,6 +4,16 @@ class Pregunta
 	include Comparable
 	attr_reader :enunciado, :respuestas, :r_correcta, :dificultad
 	def initialize(enunciado, respuestas, r_correcta , dificultad)
+		raise ArgumentError, 'El enunciado especificado no es una cadena' unless enunciado.is_a? String
+		raise ArgumentError, 'Se debe introducir un array como respuestas a la pregunta' unless respuestas.is_a? Array
+		respuestas.each{ |x|
+			raise ArgumentError, 'El array de respuestas introducido debe ser solo de cadenas' unless x.is_a? String
+		}
+		
+		raise ArgumentError, 'La solución propuesta no hace referencia a ningún índice del array de respuestas' unless r_correcta.between?(0, respuestas.size)
+
+		raise ArgumentError, 'El valor de dificultad de la pregunta no está entre 1 y 10' unless dificultad.between?(1,11) 
+
 		@enunciado = enunciado
 		@respuestas = respuestas
 		@r_correcta = r_correcta
