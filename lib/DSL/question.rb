1c2123da07629a9176d612ea20fef07ee0808000
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-12-11 19:05:28 +0000

    Clase Quiz termianda

--- lib/DSL/question.rb
+++ lib/DSL/question.rb
@@ -7,11 +7,6 @@ class Question
 
 	def initialize(text, answers)
 		@text = text
-		# "answers" is a hash.
-		# The keys "k" are pairs like [3, :right] or [6, :wrong]
-		# The first member of the pair ("6") is the answer position,
-		# the second member (":wrong"), the kind of answer.
-		# The value "v" is the answer, for instance "1942" or "1492"
 		@answers = answers.map { |k, v| Answer.new(k[INDICE], k[TIPO], v) }.sort
 	end
 
@@ -24,8 +19,6 @@ class Question
 		end
 			out
 	end
-	# ask the question and returns true if the answer is right
-	# false otherwise
 
 	def ask
 		begin
